f256d3f64da0fd513dd4edd133c3a9068fa26f70
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2021-01-13 05:20:08 +0200

    Update Write.java

--- src/stages/Write.java
+++ src/stages/Write.java
@@ -1,5 +1,19 @@
 package stages;
 
-public class Write {
+import java.util.ArrayList;
 
+import Tomasulo.Tomasulo;
+import components.ReservationStation;
+
+public class Write {
+	
+	private ReservationStation bestToBeWritten;
+	private int maxNumberOfDependencies;
+	
+	public void run() {
+		ArrayList<ReservationStation> fpAddersReadyToBeWritten = Tomasulo.fpAdders.readyToWrite();
+		ArrayList<ReservationStation> fpMultipliersReadyToBeWritten = Tomasulo.fpMultipliers.readyToWrite();
+		ArrayList<ReservationStation> loadReadyToBeWritten = Tomasulo.memory.loadReadyToWrite();
+		ArrayList<ReservationStation> storeReadyToBeWritten = Tomasulo.memory.storeReadyToWrite();
+	}
 }
