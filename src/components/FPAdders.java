a0b82e781ea0a3baf335ed1af4cbf8372b8577fb
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2021-01-13 05:19:54 +0200

    Update FPAdders.java

--- src/components/FPAdders.java
+++ src/components/FPAdders.java
@@ -1,5 +1,7 @@
 package components;
 
+import java.util.ArrayList;
+
 import Tomasulo.Tomasulo;
 
 public class FPAdders {
@@ -8,15 +10,17 @@ public class FPAdders {
 	public FPAdders() {
 		this.stations = new ReservationStation [Tomasulo.addReserveSpace];
 	}
-	
+
 	public void run() {
 		for (ReservationStation instruction : stations) {
 			if (instruction.canExec()) {
 				instruction.decCycles();
+				if (instruction.getCycles() == 0)
+					instruction.getInstruction().setExecEndCycle(Tomasulo.currentCycle);
 			}
 		}
 	}
-	
+
 	public int containsFreeStation() {
 		for (int i = 0 ; i < stations.length ; i++) {
 			ReservationStation instruction = stations[i];
@@ -25,8 +29,44 @@ public class FPAdders {
 		}
 		return -1;
 	}
-	
+
 	public void addReservation(int index,ReservationStation reservationStation) {
 		stations[index] = reservationStation;
 	}
+
+	public void clearReservation(int index) {
+		stations[index] = null;
+	}
+
+	public ArrayList<ReservationStation> readyToWrite(){
+		ArrayList<ReservationStation> list = new ArrayList<ReservationStation>();
+
+		for (ReservationStation instruction : stations) {
+			if (instruction.getCycles() == -1)
+				list.add(instruction);
+		}	
+		return list;
+	}
+
+	public ArrayList<ReservationStation> dependentOne(String nameRS){
+		ArrayList<ReservationStation> list = new ArrayList<ReservationStation>();
+
+		for (ReservationStation instruction : stations) {
+			if (instruction.getQj() == nameRS || instruction.getQk() == nameRS)
+				list.add(instruction);
+		}	
+		return list;
+	}
+	
+	public ArrayList<ReservationStation> dependentOnOnly(String nameRS){
+		ArrayList<ReservationStation> list = new ArrayList<ReservationStation>();
+
+		for (ReservationStation instruction : stations) {
+			if ((instruction.getQj() == nameRS && instruction.getQk() == null) || (instruction.getQj() == null && instruction.getQk() == nameRS))
+				list.add(instruction);
+		}	
+		return list;
+	}
 }
+
+
