a49ef42e239c7d9e67103846cff6889d7310cf5c
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2021-01-14 06:59:15 +0200

    Update Tomasulo.java

--- src/Tomasulo/Tomasulo.java
+++ src/Tomasulo/Tomasulo.java
@@ -1,37 +1,97 @@
 package Tomasulo;
 
+import java.util.ArrayList;
+
 import components.FPAdders;
 import components.FPMultipliers;
 import components.FPRegisters;
 import components.InstructionQueue;
 import components.Memory;
+import components.ReservationStation;
 import stages.Exec;
 import stages.Issue;
 import stages.Write;
+//import java.util.*;
 
 public class Tomasulo {
-	public static FPAdders fpAdders;
-	public static FPMultipliers fpMultipliers;
-	public static FPRegisters fpRegisters;
-	public static InstructionQueue instructionQueue;
-	public static Memory memory;
-	public static Issue issueStage;
-	public static Exec executeStage;
-	public static Write writeStage;
-	public static int addReserveSpace;
-	public static int mulReserveSpace;
-	public static int loadReserveSpace;
-	public static int storeReserveSpace;
-	public static int addCycles;
-	public static int subCycles;
-	public static int mulCycles;
-	public static int divCycles;
-	public static int loadCycles;
-	public static int storeCycles;
-	public static int currentCycle;
+	public static FPAdders fpAdders; // done
+	public static FPMultipliers fpMultipliers; // done
+	public static FPRegisters fpRegisters; // done
+	public static InstructionQueue instructionQueue; // done
+	public static Memory memory; // done
+	public static Issue issueStage; // done
+	public static Exec executeStage; // done
+	public static Write writeStage; // done
+	public static int addReserveSpace; // done
+	public static int mulReserveSpace; // done
+	public static int loadReserveSpace; // done
+	public static int storeReserveSpace; // done
+	public static int addCycles; // done
+	public static int subCycles; // done
+	public static int mulCycles; // done
+	public static int divCycles; // done
+	public static int loadCycles; // done
+	public static int storeCycles; // done
+	public static int currentCycle = 0; // done
+	
+	public static void run() {
+		while(!Tomasulo.instructionQueue.Done()) {
+			currentCycle++;
+			issueStage.run();
+			executeStage.run();
+			writeStage.run();
+		}
+		instructionQueue.print();
+	}
 	
 	public static void main(String[] args) {
+		// Test Case 1
+//		addReserveSpace =3;
+//		mulReserveSpace =2;
+//		loadReserveSpace =3;
+//		storeReserveSpace = 0;
+//		fpRegisters = new FPRegisters();
+//		fpAdders = new FPAdders();
+//		fpMultipliers = new FPMultipliers();
+//		memory = new Memory();
+//		issueStage = new Issue();
+//		executeStage = new Exec();
+//		writeStage = new Write();
+//		addCycles = 2;
+//		subCycles = 2;
+//		mulCycles = 10;
+//		divCycles = 40;
+//		loadCycles = 2;
+//		storeCycles = 0;
 		
+//		Test Case 2 
+		addReserveSpace =3;
+		mulReserveSpace =2;
+		loadReserveSpace =0;
+		storeReserveSpace = 0;
+		fpRegisters = new FPRegisters();
+		fpAdders = new FPAdders();
+		fpMultipliers = new FPMultipliers();
+		memory = new Memory();
+		issueStage = new Issue();
+		executeStage = new Exec();
+		writeStage = new Write();
+		addCycles = 4;
+		subCycles = 0;
+		mulCycles = 6;
+		divCycles = 9;
+		loadCycles = 0;
+		storeCycles = 0;
+		
+		instructionQueue = new InstructionQueue();
+		Tomasulo.run();
 	}
+	
+	public static void printArray(ArrayList<ReservationStation> list) {
+		for (ReservationStation i : list) {
+			System.out.println(i);
+		}
+	}
+	
 	
 }
