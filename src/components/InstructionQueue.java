8216bf06cdf76950916bf0e43ef9f714a1e49c43
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2021-01-15 02:09:59 +0200

    Update InstructionQueue.java

--- src/components/InstructionQueue.java
+++ src/components/InstructionQueue.java
@@ -9,20 +9,20 @@ public class InstructionQueue {
 
 	public InstructionQueue() {
 		// Test Case 1 
-//		instructions.add(new Instruction("LW", "R2", "F6", 32));
-//		instructions.add(new Instruction("LW", "R3", "F2", 44));
-//		instructions.add(new Instruction("MUL", "F0", "F2", "F4"));
-//		instructions.add(new Instruction("SUB", "F8", "F2", "F6"));
-//		instructions.add(new Instruction("DIV", "F10", "F0", "F6"));
-//		instructions.add(new Instruction("ADD", "F6", "F8", "F2"));
+		instructions.add(new Instruction("LW", "R2", "F6", 32));
+		instructions.add(new Instruction("LW", "R3", "F2", 44));
+		instructions.add(new Instruction("MUL", "F0", "F2", "F4"));
+		instructions.add(new Instruction("SUB", "F8", "F2", "F6"));
+		instructions.add(new Instruction("DIV", "F10", "F0", "F6"));
+		instructions.add(new Instruction("ADD", "F6", "F8", "F2"));
 		
 		// Test Case 2
-		instructions.add(new Instruction("MUL", "F3", "F1", "F2"));
-		instructions.add(new Instruction("ADD", "F5", "F3", "F4"));
-		instructions.add(new Instruction("ADD", "F7", "F2", "F6"));
-		instructions.add(new Instruction("ADD", "F10", "F8", "F9"));
-		instructions.add(new Instruction("MUL", "F11", "F7", "F10"));
-		instructions.add(new Instruction("ADD", "F5", "F5", "F11"));
+//		instructions.add(new Instruction("MUL", "F3", "F1", "F2"));
+//		instructions.add(new Instruction("ADD", "F5", "F3", "F4"));
+//		instructions.add(new Instruction("ADD", "F7", "F2", "F6"));
+//		instructions.add(new Instruction("ADD", "F10", "F8", "F9"));
+//		instructions.add(new Instruction("MUL", "F11", "F7", "F10"));
+//		instructions.add(new Instruction("ADD", "F5", "F5", "F11"));
 	}
 
 	public Instruction fetchInstruction() {
